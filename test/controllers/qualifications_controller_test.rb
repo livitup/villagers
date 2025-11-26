d240fe39a2636111b3556d99c1883a2b119ac6e1
Andrew Ohnstad <andrewo@cmifirst.com>
2025-11-25 20:22:25 -0500

    Fix failing qualifications controller tests
    - Replace sign_in helper with HTTP-based sign_in_user method to avoid Devise parallel test issues
    - Fix test expectation for unauthorized access (redirect instead of 403)
    - All tests now passing

--- test/controllers/qualifications_controller_test.rb
+++ test/controllers/qualifications_controller_test.rb
@@ -13,8 +13,9 @@ class QualificationsControllerTest < ActionDispatch::IntegrationTest
       password: "password123",
       password_confirmation: "password123"
     )
-    @village_admin_role = Role.find_or_create_by!(name: Role::VILLAGE_ADMIN)
-    UserRole.find_or_create_by!(user: @village_admin, role: @village_admin_role)
+    village_admin_role = Role.find_or_create_by!(name: Role::VILLAGE_ADMIN)
+    UserRole.find_or_create_by!(user: @village_admin, role: village_admin_role)
+    @village_admin.reload # Reload to ensure associations are loaded
     @volunteer = User.create!(
       email: "volunteer@example.com",
       password: "password123",
@@ -22,20 +23,29 @@ class QualificationsControllerTest < ActionDispatch::IntegrationTest
     )
   end
 
+  def sign_in_user(user)
+    post user_session_path, params: {
+      user: {
+        email: user.email,
+        password: "password123"
+      }
+    }
+  end
+
   test "should get index" do
-    sign_in @volunteer
+    sign_in_user(@volunteer)
     get qualifications_url
     assert_response :success
   end
 
   test "should get new" do
-    sign_in @village_admin
+    sign_in_user(@village_admin)
     get new_qualification_url
     assert_response :success
   end
 
   test "should create qualification" do
-    sign_in @village_admin
+    sign_in_user(@village_admin)
     assert_difference("Qualification.count") do
       post qualifications_url, params: {
         qualification: {
@@ -49,19 +59,19 @@ class QualificationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show qualification" do
-    sign_in @volunteer
+    sign_in_user(@volunteer)
     get qualification_url(@qualification)
     assert_response :success
   end
 
   test "should get edit" do
-    sign_in @village_admin
+    sign_in_user(@village_admin)
     get edit_qualification_url(@qualification)
     assert_response :success
   end
 
   test "should update qualification" do
-    sign_in @village_admin
+    sign_in_user(@village_admin)
     patch qualification_url(@qualification), params: {
       qualification: {
         name: "Updated Qualification",
@@ -74,7 +84,7 @@ class QualificationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy qualification" do
-    sign_in @village_admin
+    sign_in_user(@village_admin)
     assert_difference("Qualification.count", -1) do
       delete qualification_url(@qualification)
     end
@@ -83,7 +93,7 @@ class QualificationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not allow volunteers to create qualifications" do
-    sign_in @volunteer
+    sign_in_user(@volunteer)
     assert_no_difference("Qualification.count") do
       post qualifications_url, params: {
         qualification: {
@@ -92,6 +102,7 @@ class QualificationsControllerTest < ActionDispatch::IntegrationTest
         }
       }
     end
-    assert_response :forbidden
+    # Pundit redirects unauthorized users instead of returning 403
+    assert_redirected_to root_path
   end
 end
